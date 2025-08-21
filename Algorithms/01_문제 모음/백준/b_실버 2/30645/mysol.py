e8c46310dfedc35cbe2cdf552053041286a8d00d
LWH <lwh4sty@gmail.com>
2025-08-21 10:19:14 +0900

    30645 다시 풀기 성공

---
+++ Algorithms/01_문제 모음/백준/b_실버 2/30645/mysol.py
@@ -0,0 +1,80 @@
+import sys, time
+sys.stdin = open("input.txt")
+start_time = time.time()
+
+#==================================================
+T = int(input())
+
+for tc in range(1, T + 1):
+    R, C = map(int, input().split())
+    N = int(input())
+    height = list(map(int, input().split()))
+
+    # 인형을 높이 순서대로 정렬.
+    height = sorted(height, reverse=True)
+
+    # 그래프 생성
+    graph = [[0] * C for _ in range(R)]
+
+    # 포인터 활용, 인형을 기록하기 위해
+    p = 0
+    flag = True
+
+    # graph에 인형 적재
+    for r in range(R):
+        for c in range(C):
+            flag = True
+            
+            # 노드에 적합한 인형 추가.
+            while flag:
+                if p >= N:
+                    break
+                
+                # p < C인 경우 그냥 추가.
+                if r == 0:
+                    graph[r][c] = height[p]
+                    flag = False
+                    p += 1
+                
+                elif r >= 1 and graph[r-1][c] != height[p]:
+                    graph[r][c] = height[p]
+                    flag = False
+                    p += 1
+                
+                else:
+                    p += 1
+
+    # 중복을 제거하고 인형을 배치하였기에 count만하면 된다.
+    Ans = 0
+    for r in range(R):
+        for c in range(C):
+            
+            if graph[r][c] == 0:
+                continue
+            Ans += 1
+    print(Ans)
+#==================================================
+
+end_time = time.time()
+print("time: ", end_time - start_time)
+
+'''
+< 시간 복잡도 >
+- 정렬: N
+
+- 비교연산: height의 길이 (100,000)
+
+- Ans 연산: R * C
+'''
+
+'''
+< 로 직 >
+- 가장 뒤의 행부터 가장 큰 순서대로 인형이 배치된다.
+
+- 인형을 내림차순으로 정렬하고 graph에 배치를 시작한다.
+
+- 인형을 배치할 때에는 키가 동일한 경우는 skip해야 한다.
+    - 이유는 인형의 수가 graph의 크기보다 많은 경우 문제가 되기 때문이다.
+
+- 배치를 완료하였다면 0이 아닌(즉, 인형이 배치된) 칸의 수만 count하면 된다.
+'''
---
+++ SQL/프로그래머스/Lv. 01/29_가장 큰 물고기 10마리 구하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+SELECT ID, LENGTH
+FROM FISH_INFO
+-- 길이 내림차순, ID 오름차순 정렬
+ORDER BY LENGTH DESC, ID ASC
+-- 10개 출력
+LIMIT 10
