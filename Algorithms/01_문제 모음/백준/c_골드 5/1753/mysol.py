42e4f3493791814b8ef7aa620d0c3952d6906b6f
lwh <lwh4sty@gmail.com>
2026-01-17 00:35:30 +0900

    1753

---
+++ Algorithms/01_문제 모음/백준/c_골드 5/1753/mysol.py
@@ -0,0 +1,10 @@
+import sys, time
+sys.stdin = open("input.txt")
+start = time.time()
+
+#======================================================
+
+#======================================================
+
+end = time.time()
+print("time: ", end - start)
