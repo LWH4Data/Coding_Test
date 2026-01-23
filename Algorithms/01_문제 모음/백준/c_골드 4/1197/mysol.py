2b0adbf87b89416366ab3d9110d8b6f6e7882517
lwh <lwh4sty@gmail.com>
2026-01-23 17:10:33 +0900

    1197번 MST 프림 템플릿이 필요해 정리하였다.

--- Algorithms/01_문제 모음/백준/c_골드 4/1197/mysol.py
+++ Algorithms/01_문제 모음/백준/c_골드 4/1197/mysol.py
@@ -3,48 +3,51 @@ sys.stdin = open("input.txt")
 s_t = time.time()
 
 #==================================================================
-
-# find 함수
-def find(a):
-    if parent[a] == a:
-        return a
-    
-    while a != parent[a]:
-        a = parent[a]
-        parent[a] = a
-    return a
-
-# union 함수
-def union(a, b):
-    a = find(a)
-    b = find(b)
-
-    if a != b:
-        parent[b] = a
+# MST를 프림으로 구현한다.
+#   ● 크루스컬로 구현하면 Python에서는 recursion 문제가 발생할 수 있다.
+#   ● 다익스트라는 특정 노드에서 다른 노드까지의 최단 경로(누적합)을 탐색하기에
+#     프림처럼 pop 시점에 visited를 확인하지 않는다.
+#   ● 프림은 단일 간선 정보를 활용하기에 중복 노드가 나온다면 이미 해당 노드의
+#     최단 경로를 확인하였기에 visited를 체크한다.
+from collections import defaultdict
+import heapq
 
 N, E = map(int, sys.stdin.readline().split())
-edges = []
+graph = defaultdict(list)
 for _ in range(E):
-    edges.append(list(map(int, sys.stdin.readline().split())))
-edges = sorted(edges, key=lambda x: x[2])
-parent = [0] * (N + 1)
-for i in range(N + 1):
-    parent[i] = i
+    s, e, w = map(int, sys.stdin.readline().split())
+    graph[s].append((w, e))
+    graph[e].append((w, s))
+visited = [False] * (N + 1)
 
-ans = 0
-check = 0
-c = 0
-while c != N - 1:
-    s, e, w = edges[check]
-    if find(s) == find(e):
-        check += 1
-    else:
-        union(s, e)
-        check += 1
-        c += 1
-        ans += w
+q = []
+heapq.heappush(q, (0, 1))
 
-print(ans)
+# MST의 전체 비용
+result = 0
+# MST에 포함된 노드 수
+cnt = 0
+
+# 전체를 탐색
+while q:
+    nw, nn = heapq.heappop(q)
+
+    # 방문한 노드라면 skip
+    #   ● 이미 해당 노드의 최소 간선을 방문했기에 확인하지 않는다.
+    if visited[nn]:
+        continue
+    visited[nn] = True
+
+    # 방문하지 않았다면 최소 가중치 누적합
+    result += nw
+
+    # 다음 노드를 순회하면서 업데이트
+    for cw, cn in graph[nn]:
+        if visited[cn]:
+            continue
+        heapq.heappush(q, (cw, cn))
+    
+print(result)
 #==================================================================
 
 e_t = time.time()
