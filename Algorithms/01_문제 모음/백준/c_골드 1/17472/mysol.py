0b0dd17c578bd668a851ba77520d5f3362b2d27c
lwh <lwh4sty@gmail.com>
2026-01-24 23:15:34 +0900

    17472: MST 로직을 크루스컬에서 프림으로 변경

--- Algorithms/01_문제 모음/백준/c_골드 1/17472/mysol.py
+++ Algorithms/01_문제 모음/백준/c_골드 1/17472/mysol.py
@@ -47,21 +47,11 @@ def BFS(i, j):
     while queue:
         r, c = queue.popleft()
         for d in range(4):
-            tempR = dr[d]
-            tempC = dc[d]
-            while r + tempR >= 0 and r + tempR < N and c + tempC >= 0 and c + tempC < M:
-                if not visited[r + tempR][c + tempC] and myMap[r + tempR][c + tempC] != 9:
-                    addNode(r + tempR, c + tempC, queue)
-                else:
-                    break
-                if tempR < 0:
-                    tempR -= 1
-                elif tempR > 0:
-                    tempR += 1
-                elif tempC < 0:
-                    tempC -= 1
-                elif tempC > 0:
-                    tempC += 1
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if 0 <= nr < N and 0 <= nc < M:
+                if not visited[nr][nc] and myMap[nr][nc] != 0:
+                    addNode(nr, nc, queue)
     
     return mlist
 
@@ -76,7 +66,8 @@ for i in range(N):
             # 새로운 섬 넘버링
             sumlist.append(tempList)
 
-pq = PriorityQueue()
+# 인접 리스트: 각 섬에서 다른 섬으로 가는 최소 거리 저장
+edges = [[] for _ in range(sNum)]
 
 # 섬의 각 지점에서 만들 수 있는 모든 에지를 저장
 for now in sumlist:
@@ -85,7 +76,7 @@ for now in sumlist:
         r = temp[0]
         c = temp[1]
         now_S = myMap[r][c]
-        # 네 방향 탐색 → 우선순위 큐에 에지 정보 저장
+        # 네 방향 탐색 → 인접 리스트에 에지 정보 저장
         for d in range(4):
             tempR = dr[d]
             tempC = dc[d]
@@ -98,7 +89,7 @@ for now in sumlist:
                 elif myMap[r + tempR][c + tempC] != 0:
                     # 다른 섬 → 길이가 1 초과일 때 에지로 추가.
                     if blength > 1:
-                        pq.put((blength, now_S, myMap[r + tempR][c + tempC]))
+                        edges[now_S].append((blength, myMap[r + tempR][c + tempC]))
                     break
                 # 바다인 경우 다리의 길이 연장
                 else: 
@@ -112,40 +103,39 @@ for now in sumlist:
                 elif tempC > 0:
                     tempC += 1
 
-# find 함수 구현
-def find(a):
-    if a == parent[a]:
-        return addNode
-    else:
-        parent[a] = find(parent[a])
-        return parent[a]
+# Prim 알고리즘으로 최소 신장 트리 구현
+visitedIsland = [False] * sNum
+pq = PriorityQueue()
 
-# union 함수 구현
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a != b:
-        parent[b] = a
-
-# 자기 자신을 대표 노드로 초기화
-parent = [0] * sNum
-for i in range(len(parent)):
-    parent[i] = i
-
-useEdge = 0
+# 1번 섬부터 시작 (섬 번호는 1부터 시작)
+visitedIsland[1] = True
+connected = 1  # 연결된 섬의 개수
 result = 0
 
-# 최소 신장 트리 알고리즘 수행
-while pq.qsize() > 0:
-    v, s, e = pq.get()
-    # 연결해도 사이클이 생기지 않으면
-    if find(s) != find(e):
-        union(s, e)
-        result += visited
-        useEdge += 1
+# 1번 섬과 연결된 모든 간선을 우선순위 큐에 추가
+for edge in edges[1]:
+    pq.put(edge)  # (거리, 목적지 섬)
 
-# sNum이 실제 섬의 수보다 1 크기 때문에 섬의 번호 표시를 위해 -2로 연산
-if useEdge == sNum - 2:
+# Prim 알고리즘 수행
+while pq.qsize() > 0 and connected < sNum - 1:
+    dist, next_island = pq.get()
+    
+    # 이미 방문한 섬이면 건너뛰기
+    if visitedIsland[next_island]:
+        continue
+    
+    # 새로운 섬을 MST에 추가
+    visitedIsland[next_island] = True
+    result += dist
+    connected += 1
+    
+    # 새로 추가된 섬과 연결된 모든 간선을 우선순위 큐에 추가
+    for edge in edges[next_island]:
+        if not visitedIsland[edge[1]]:
+            pq.put(edge)
+
+# sNum이 실제 섬의 수보다 1 크기 때문에 섬의 번호 표시를 위해 -1로 연산
+if connected == sNum - 1:
     print(result)
 else:
     print(-1)
