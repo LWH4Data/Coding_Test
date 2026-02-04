311e155f2a29479da082a3ac4b1e36ca3c0f5cf7
lwh <lwh4sty@gmail.com>
2026-02-04 17:22:29 +0900

    13251: 점화식? 규칙? 도출하여 풀이

---
+++ Algorithms/01_문제 모음/백준/b_실버 3/13251_조약돌_꺼내기/mysol.py
@@ -0,0 +1,38 @@
+import sys, time
+sys.stdin = open("input.txt")
+s_t = time.time()
+
+#=====================================================================
+# 입력값 받기
+M = int(sys.stdin.readline())
+colors = list(map(int, sys.stdin.readline().split()))
+K = int(sys.stdin.readline())
+N = sum(colors)
+
+# 1개반 뽑는 경우 예외처리
+flag = False
+if K == 1:
+    flag = True
+
+# 색상이 1개인 경우 예외처리
+if M == 1:
+    flag = True
+
+# 색상 리스트의 M개의 원소마다 K번의 확률을 계산하고 더한다.
+# 연산 수행으로 답 구하기
+result = 0
+temp = 0
+for m in range(M):
+    result += temp
+    temp = 1
+    for k in range(K):
+        temp *= (colors[m] - k) / (N - k)
+
+
+print(result + temp)
+
+# 점화식 
+# print((5/18)*(4/17) + (6/18)*(5/17) + (7/18)*(6/17))
+#=====================================================================
+
+e_t =time.time()
