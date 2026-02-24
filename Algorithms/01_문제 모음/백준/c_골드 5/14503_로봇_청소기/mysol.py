6df69cba4d334a59050a5c4596fba4562f82698b
lwh <lwh4sty@gmail.com>
2026-02-25 01:47:14 +0900

    14503: 문제 설명이... 2번과 3번의 설명이 이해하기 어려웠음

--- Algorithms/01_문제 모음/백준/c_골드 5/14503_로봇_청소기/mysol.py
+++ Algorithms/01_문제 모음/백준/c_골드 5/14503_로봇_청소기/mysol.py
@@ -3,10 +3,9 @@ sys.stdin = open("input.txt")
 s_t = time.time()
 
 #=====================================================================
-# 델타로직
-dv = [-1, 0, 1, 0]
-dh = [0, 1, 0, -1]
-
+# 방향에 맞는 델타 로직
+dv = (-1, 0, 1, 0)
+dh = (0, 1, 0, -1)
 
 # 입력값 받기
 N, M = map(int, sys.stdin.readline().split())
@@ -14,45 +13,52 @@ v, h, d = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 
-# 입력을 반복
+# 청소 로직 실행
 check = True
 cnt = 0
 while check:
 
-    # 청소 되지 않았다면 청소
+    # 현재 칸이 청소되지 않은 칸이면 청소.
     if not visited[v][h]:
-        visited[v][h] = True
+        visited[v][h] = True 
         cnt += 1
-    
-    # 주변 탐색
-    temp = False
+
+    # 청소되지 않은 빈 칸이 있는 경우.
+    three = False
     for i in range(4):
         nv = v + dv[i]
         nh = h + dh[i]
 
-        # 청소되지 않은 빈 칸이 있는 경우.
-        if not visited[nv][nh] and graph[nv][nh] != 1:
+        if 0 <= nv < N and 0 <= nh < M and not visited[nv][nh] and graph[nv][nh] == 0:
+            three = True
             
-            # 반 시계 방향으로 회전
-            d = (d - 1) % 4
+            # 반시계로 회전했을 때 앞이 전진 가능한 만큼 반복
+            temp = 0
+            while temp != 4:
+                # 반시계로 90도 회전
+                d = (d - 1) % 4
 
-            # 바라 보는 방향의 앞쪽 칸이 청소되지 않았다면 전진.
-            if not visited[v + dv[d]][h + dh[d]] and graph[v + dv[d]][h + dh[d]] != 1:
-                v = v + dv[d]
-                h = h + dh[d]
-                temp = True
+                # 바라 보는 칸이 청소되지 않은 빈 칸이면 전진
+                if not visited[v + dv[d]][h + dh[d]] and graph[v + dv[d]][h + dh[d]] == 0:
+                    v = v + dv[d]
+                    h = h + dh[d]
+                    # 종료
+                    temp  = 4
+                # 아니라면 한 번 더 회전
+                else:
+                    temp += 1
+            break
 
-    if temp:
-        continue
+    # 청소되지 않은 빈 칸이 없는 경우.
+    if not three:
 
-    # 후진 가능하다면 후진
-    if graph[v + dv[(d + 2) % 4]][h + dh[(d + 2) % 4]] != 1:
-        v = v + dv[(d + 2) % 4]
-        h = h + dh[(d + 2) % 4]
-    
-    # 아니라면 탐색 중지
-    else:
-        check = False
+        # 후진이 가능하다면 후진
+        if graph[v + dv[(d + 2) % 4]][h + dh[(d + 2) % 4]] == 0:
+            v = v + dv[(d + 2) % 4]
+            h = h + dh[(d + 2) % 4]
+        # 불가하다면 종료
+        else:
+            check = False
 
 print(cnt)
 #=====================================================================
