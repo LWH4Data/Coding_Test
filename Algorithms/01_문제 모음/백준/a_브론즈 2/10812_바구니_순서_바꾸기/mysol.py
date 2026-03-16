58f510e9d5e9c5941751d4da382b1d30e3cd52a7
lwh <lwh4sty@gmail.com>
2026-03-16 16:29:09 +0900

    10812: 리스트 순서를 바꾸는 법 학습.

---
+++ Algorithms/01_문제 모음/백준/a_브론즈 2/10812_바구니_순서_바꾸기/mysol.py
@@ -0,0 +1,28 @@
+import sys, time, copy
+sys.stdin = open("input.txt")
+s_t = time.time()
+
+#===============================================================
+N, M = map(int, sys.stdin.readline().split())
+
+nums = [0] * (N + 1)
+for i in range(N + 1):
+    nums[i] = i
+
+for _ in range(M):
+    start, end, mid = map(int, sys.stdin.readline().split())
+    temp_start_mid = nums[start : mid].copy()
+    temp_mid_end = nums[mid : end + 1].copy()
+    print(temp_start_mid)
+   
+    for i in range(start, start + end + 1 - mid):
+        nums[i] = temp_mid_end[i - 1]
+    for i in range(start + end + 1 - mid, end + 1):
+        print(i)
+        nums[i] = temp_start_mid[i - 1]
+    
+    break
+#===============================================================
+
+e_t = time.time()
+print("time: ", e_t - s_t)
